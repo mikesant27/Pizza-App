24e29c93ea765db23f2817627d74c4187db3dfe3
Michael <mikesant27@gmail.com>
2025-02-14 20:16:46 -0500

    renamed activity and layout

---
+++ app/src/main/java/com/example/homework2/PizzaSelectionActivity.kt
@@ -0,0 +1,17 @@
+package com.example.homework2
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class PizzaSelectionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_pizza_selection)
+
+
+    }
+}
